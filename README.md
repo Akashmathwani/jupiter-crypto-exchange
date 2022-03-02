ea899fa7f77934602626e39ca00c4ef0838446a0
Akash Mathwani <2015eeb1045@iitrpr.ac.in>
2022-03-02 20:24:51 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+## To Run the Project
 
+- Run `npm install`
+- There seems to be a problem in jupitor core library so go to `./node_modules/@jup-ag/core/dist/core.esm.js` line 4365 and fix the timeout from 45_000 to 45000 and 20_000 to 20000
+- Run `npm run start` to Run the Project
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { JupiterProvider } from '@jup-ag/react-hook'
-import { Connection } from '@solana/web3.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { JupiterProvider } from "@jup-ag/react-hook";
+import { Connection } from "@solana/web3.js";
 
-declare let module: any
-
-const connection = new Connection('https://solana-api.projectserum.com')
+const connection = new Connection("https://solana-api.projectserum.com");
 
 ReactDOM.render(
   <React.StrictMode>
-    <JupiterProvider connection={connection} cluster={'mainnet-beta'}>
+    <JupiterProvider connection={connection} cluster={"mainnet-beta"}>
       <App />
     </JupiterProvider>
   </React.StrictMode>,
 
-  document.getElementById('root')
-)
-if (module.hot) {
-  module.hot.accept()
-}
+  document.getElementById("root")
+);
